628a8c425216cd43af4f4f28585a8529401095dd
jhartung <joel.hartung@gmail.com>
2022-07-17 21:02:32 -0500

    Assignment 8.3

---
+++ src/app/loan-calculator/loan-calculator.component.ts
@@ -0,0 +1,25 @@
+/*
+============================================
+; Title: Assignment 8.3 - Capstone Build
+; Author: Professor Krasso
+; Date: 17 July 2022
+; Modified By: Joel Hartung
+; Title: Assignment 8.3 - Capstone Build
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-calculator',
+  templateUrl: './loan-calculator.component.html',
+  styleUrls: ['./loan-calculator.component.css']
+})
+export class LoanCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
